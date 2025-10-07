e3ac628b6351fb446f73c18dccb4bd8957cc69e8
VirajBuwaneka <virajpathirana35@gmail.com>
2025-10-08 01:32:16 +0530

    new

--- hungry.py
+++ hungry.py
@@ -3,7 +3,7 @@ if hungry=="Yes":
     print("eat samosa")
     print("eat burger")
     print("eat pizza")
-    print("eat ice cream")
+    
 else:
     thirsty=input("are you thirsty?")
     if thirsty=="Yes":
